d2331a4c164758e42f832ba5027ca476fd8b784c
Syunpon <32010151+Syunpon@users.noreply.github.com>
2022-08-07 10:55:49 +0900

    結果にコミット

--- js/make_room.js
+++ js/make_room.js
@@ -1,7 +1,6 @@
 document.getElementById("makeRoomButton").onclick = () =>{
-    console.log("Hello World!\n")
-    //newRoom = 
-    //roomID = newRoom.roomID
-    roomID = "abc"
-    location.href = "room.html?roomID=" + encodeURIComponent(roomID)
+    console.log("Hello World!\n");
+    //roomID = await sendRoomInfo();
+    roomID = "abc";
+    location.href = "room.html?roomID=" + encodeURIComponent(roomID);
 }
