941952f3b45a6445d1aa8deb34777bd216a3b353
MasakazuHarumoto <108621266+MasakazuHarumoto@users.noreply.github.com>
2022-08-07 11:24:22 +0900

    Delete js directory

--- js/make_room.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("makeRoomButton").onclick = () =>{
-    console.log("Hello World!\n");
-    //roomID = await sendRoomInfo();
-    roomID = "abc";
-    location.href = "room.html?roomID=" + encodeURIComponent(roomID);
-}
