016c13f9a55fea527d611d7180abff8320844975
yukihanawa <hanawayuki@hanawayukinoMacBook-Air.local>
2022-08-07 12:32:42 +0900

    [メッセージ編集機能追加中]

--- message_send.js
+++ message_send.js
@@ -1,4 +1,22 @@
-function clickBtn() {
-    const t1 = document.getElementById("message").value;
-    document.getElementById("span").textContent = t1;
+//メッセージ送信関数
+function clickBtn_send() {
+    //t1にメッセージを入れる
+    const t1 = document.getElementById("messagebox").value;
+    document.getElementById("message").textContent = t1;
+    //sendMsgInfo(roomID, t1);
+    console.log(t1);
+    t3 = document.getElementById("m_send").textContent;
+    if (t3 === "編集") {
+        document.getElementById("m_send").textContent = "送信"
+    }
+}
+//メッセージ編集関数（文字がクリックされたとき）
+function clickBtn_choice() {
+    const t2 = document.getElementById("message").textContent;
+    console.log(t2);
+    document.getElementById("messagebox").value = t2;
+    document.getElementById("m_send").textContent = "編集";
+    document.getElementById("m_send").onclick = clickBtn_edit;
+
+    console.log("clickBtn_edit");
 }
