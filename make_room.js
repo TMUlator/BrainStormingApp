b890f318ca3257d89d1bdabad79f0cf95f5892bd
yukihanawa <92089581+yukihanawa@users.noreply.github.com>
2022-08-07 21:10:58 +0900

    Delete make_room.js

--- make_room.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("makeRoomButton").onclick = async () =>{
-    console.log("Hello World!\n");
-    roomID = await sendRoomInfo();
-    //roomID = "abc";
-    location.href = "room.html?roomID=" + encodeURIComponent(roomID);
-}
